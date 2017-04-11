431e15c94a6d9a41e958f4759a567b31872ea5bf
Izzy Hasson <izzyhasson@att.net>
2017-04-11 16:22:10 -0700

    Added a merge hs and college info function.

---
+++ readData.py
@@ -0,0 +1,172 @@
+# This script reads data and strips some of the irrelevant crap from
+# the the second field. Also finds best match from a list. 
+#
+#    Copyright (C) 2017 Izzy Hasson <izzy.hasson.925@my.csun.edu>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+
+
+from fuzzywuzzy import StringMatcher as strM
+import re
+import sys
+
+class Student:
+
+    def __init__(self,sid):
+        self.sid = sid
+        self.hsCourses = []
+        self.collegeSeq = []
+
+# Returns a list of Students with their highschool classes.
+# Will make the adding students to dictionary and figuring out what the
+# courseString maps to in different function.
+def read_High_School_Data(filesList):
+    studentList = []
+    errors = 0
+    for filename in filesList:
+        with open(filename, 'r') as f:
+            linenum = 0 # for errorprinting
+            #format studentid==|course|??|??|
+            for line in f.readlines():
+                print(line)
+                linenum +=1
+                line = line.rstrip()
+                idstring,rest = line.split("==|")
+                newStudent = Student(idstring)
+                vals = []
+                vals = rest.split('|')
+                #since each course should have a name and <=two grades
+                if (len(vals)-1)%3 != 0:
+                    #print(vals)
+                    errors += 1
+                elif False:
+                    print( "Misformatted Input!  " + 
+                        "line "+str(linenum)+ " in " + filename)
+                else : 
+                    for i in range(int(len(vals)/3)):
+                        acourse = (vals[3*i+0], vals[3*i+1],vals[3*i+2])
+                        newStudent.hsCourses.append(acourse)
+                    studentList.append(newStudent)
+    return studentList
+
+# read data and also find what appears to be the closest match for the 
+# course name.
+def readAndClean(fileList):
+    dataset = []
+    for fname in fileList:
+        with open(fname,'r') as f:
+            for line in f:
+                vals = line.split('|')
+                cname = vals[1]
+                cname = cname.replace('II','2')
+                cname = cname.lower()
+                cname = re.sub("\(.*\)","",cname)
+                cname = re.sub("common core","",cname)
+                cname = re.sub("honors","",cname)
+                cname = re.sub("ap ","",cname)
+                cname = re.sub("h(\.)? ","",cname)
+                cname.strip()
+                closest = findClosest(cname)
+                dataset.append((vals,cname,closest))
+                print( cname +"\t" + closest )
+    return dataset
+
+def justClean(string):
+    cname = string
+    cname = cname.replace('II','2')
+    cname = cname.lower()
+    cname = re.sub("\(.*\)","",cname)
+    cname = re.sub("common core","",cname)
+    cname = re.sub("honors","",cname)
+    cname = re.sub("ap ","",cname)
+    cname = re.sub("h(\.)? ","",cname)
+    cname.strip()
+    return cname
+
+def findClosest(string):
+    knownclasses = ['algebra 1', 'algebra 2', 'geometry', 'calculus',
+                    'pre-calculus', 'trigenometry', 'statistics',
+                    'math analysis', 'trig/alg', 'alg/trig', 'algebra']
+    bestMatch = "unknown"
+    bestdistance = strM.distance(bestMatch,string)
+    for c in knownclasses:
+        dist = strM.distance(c,string) 
+        if dist < bestdistance :
+            bestdistance = dist
+            bestMatch = c
+    return bestMatch
+
+def read_a_sequence(line):
+    line = line.strip()
+    sid,seq = line.split("==|")
+    fields = seq.split("|")
+    cInfoList = []
+    if len(fields)%4 == 0:
+        numbOfCourses = int(len(fields)/4)
+        for x in range(numbOfCourses):
+            cInfo = []
+            cInfo.append(fields[(4*x)+0])
+            cInfo.append(fields[(4*x)+1])
+            cInfo.append(fields[(4*x)+2])
+            cInfo.append(fields[(4*x)+3])
+            cInfoList.append(cInfo)
+        return (sid,cInfoList)
+    else:
+        print("Malformed Course Sequence! Not enough fields.")
+        return None
+
+#Both inputs should be dictionaries filled with Student objects.
+def mergeHSandColSeq(hs,cseq):
+    mergedDict = {}
+    for student in hs.keys():
+        if student in cseq:
+            if cseq[student].collegeSeq and hs[student].hsCourses:
+                csunStudent = Student(student)
+                csunStudent.hsCourses = hs[student].hsCourses
+                csunStudent.collegeSeq = cseq[student].collegeSeq
+                mergedDict[student] = csunStudent
+    return mergedDict
+
+def get_sequences(fileName):
+    sList = []
+    with open(fileName, 'r') as f:
+        for line in f.readlines():
+            sList.append(read_a_sequence(line))
+    return sList
+
+def constructDictionary():
+    studentDir = {}
+    highschoolfiles = ['encrypted-grant-grades.txt',
+            'encrypted-poly-grades.txt',
+            'encrypted-taft-grades.txt']
+    seqFile = 'Encrypted-Math-Sequences.txt'
+    highschooldata = read_High_School_Data(highschoolfiles)
+    seqList = get_sequences(seqFile)
+    for student in highschooldata:
+        studentDir[student.sid] = student
+    for sid,seq in seqList:
+        if sid in studentDir:
+            student = studentDir[sid] 
+            student.collegeSeq = seq
+    return studentDir
+
+            
+
+def showall(d):
+    for x in d.keys():
+        a = d[x]
+        print(a.sid)
+        print(a.hsCourses)
+        print(a.collegeSeq)
