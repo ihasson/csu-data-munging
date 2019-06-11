a1137cd0b9f4bc3333d3c11a46d59c47fba8cc23
Izzy Hasson <izzyhasson@att.net>
2019-06-11 07:45:51 -0700

    sql files

--- database/load.sql
+++ database/load.sql
@@ -3,3 +3,6 @@ INTO TABLE hs_courses_and_grades
 COLUMNS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
+
+
+\copy exams from '/home/izzy/data_science/csv/full-exams-database.csv' DELIMITER'|' CSV HEADER;
